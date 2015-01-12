7d6ba4f44aa967cffcb362ba7d6473b4a859d215
Morten Meilby <mm@iserve.dk>
2015-01-12 21:21:36 +0100

    Admin login fix

--- src/TrainingCompany/QueryBundle/Controller/Admin/LoginController.php
+++ src/TrainingCompany/QueryBundle/Controller/Admin/LoginController.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\HttpFoundation\Request;
 
 class LoginController extends Controller
 {
@@ -12,21 +13,43 @@ class LoginController extends Controller
      * @Route("/admin/login", name="_admin_login")
      * @Template("TrainingCompanyQueryBundle:Default:login.html.twig")
      */
-    public function loginAction()
+    public function loginAction(Request $request)
     {
-        $request = $this->getRequest();
         if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
             $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
         } else {
             $error = $request->getSession()->get(SecurityContext::AUTHENTICATION_ERROR);
         }
 
+        $form = $this->makeLoginForm($request);
+        $form->handleRequest($request);
+        
         return array(
+            'form'          => $form->createView(),
             'last_username' => $request->getSession()->get(SecurityContext::LAST_USERNAME),
-            'error'         => $error,
+            'error'         => $error
         );
     }
 
+    private function makeLoginForm(Request $request) {
+        $formDef = $this->createFormBuilder(array('username' => $request->getSession()->get(SecurityContext::LAST_USERNAME)));
+        $formDef->setAction($this->generateUrl('_security_check'));
+        $formDef->add('username', 'text', array('label' => 'Brugernavn',
+                                                'translation_domain' => 'club',
+                                                'required' => false,
+                                                'help' => 'Indtast dit brugernavn',
+                                                'icon' => 'fa fa-user'));
+        $formDef->add('password', 'password', array('label' => 'Kodeord', 
+                                                    'translation_domain' => 'club',
+                                                    'required' => false,
+                                                    'help' => 'Indtast kodeord',
+                                                    'icon' => 'fa fa-key'));
+        $formDef->add('login', 'submit', array('label' => 'Log ind',
+                                               'translation_domain' => 'club',
+                                               'icon' => 'fa fa-sign-in'));
+        return $formDef->getForm();
+    }
+    
     /**
      * @Route("/admin/login_check", name="_security_check")
      */
